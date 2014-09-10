2fbd35bcada29186764b97b3b910115a47d5e3bd
Arturs Vilkajs <arturs.vilkajs@wunderkraut.com>
2014-09-10 19:41:33 +0300

    Inital commit

---
+++ conf/vagrant.settings.php
@@ -0,0 +1,19 @@
+<?php
+
+// DATABASE
+
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'drupal',
+      'username' => 'drupal',
+      'password' => 'password',
+      'host'     => 'localhost',
+      'port'     => '',
+      'driver'   => 'mysql',
+      'prefix'   => '',
+    ),
+  ),
+);
