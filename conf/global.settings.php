2a78b3b963920cef3339b07e58a87323417f51e7
Arturs Vilkajs <arturs.vilkajs@wunderkraut.com>
2014-09-11 16:37:45 +0300

    DB settings stub added to global.settings.php file

--- conf/global.settings.php
+++ conf/global.settings.php
@@ -1,2 +1,18 @@
 <?php
 
+//$databases = array (
+//  'default' =>
+//  array (
+//    'default' =>
+//    array (
+//      'database' => 'drupal',
+//      'username' => 'drupal',
+//      'password' => 'password',
+//      'host' => 'localhost',
+//      'port' => '',
+//      'driver' => 'mysql',
+//      'prefix' => '',
+//    ),
+//  ),
+//);
+
